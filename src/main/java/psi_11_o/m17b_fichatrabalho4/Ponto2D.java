053eef0bad96b4543e4d3579351c9437cb6e6fc5
Francisco Paulos <>
2024-01-30 12:12:37 +0000

    segundo commit com os gettes e settes e o javadoc

--- src/main/java/psi_11_o/m17b_fichatrabalho4/Ponto2D.java
+++ src/main/java/psi_11_o/m17b_fichatrabalho4/Ponto2D.java
@@ -8,18 +8,60 @@ public class Ponto2D {
         x = 0;
         y = 0;        
     }
+    /**
+     * 
+     * @param x
+     * @param y 
+     */
     
     public Ponto2D(int x, int y){
         this.x = x;
         this.y = y;        
     }
+    /**
+     * 
+     * @param a
+     * @return 
+     */
     
     public double distancia(Ponto2D a){
-        return Math.abs(Math.sqrt(Math.pow((x-a.x),2)+Math.pow((y-a.y),2)));
+        return Math.abs(Math.sqrt(Math.pow((getX()-a.getX()),2)+Math.pow((getY()-a.getY()),2)));
     }
+    /**
+     * 
+     * @return 
+     */
 
     @Override
     public String toString() {
-        return "Ponto2D[" + x + "," + y + "]";
+        return "Ponto2D[" + getX() + "," + getY() + "]";
     }    
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
 }
